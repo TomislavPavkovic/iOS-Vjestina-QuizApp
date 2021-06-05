bf5a8b974c23fc00b9d6a7991bb4ee6254b0185c
Tomislav Pavkovic <tomislav.pavkovic@fer.hr>
2021-06-05 14:57:31 +0200

    predaja 5. zadace

--- QuizApp/Presenter/LoginPresenter.swift
+++ QuizApp/Presenter/LoginPresenter.swift
@@ -29,13 +29,15 @@ class LoginPresenter {
                 switch result {
                 case .failure(let error):
                     print(error)
-                    return completion(.error(1, "Request error"))
+                    completion(.error(1, "Request error"))
                 case .success(let value):
                     let userDefaults = UserDefaults.standard
                     userDefaults.set(value.token, forKey: "token")
                     userDefaults.set(value.userId, forKey: "userId")
-                    self.router.showQuizzesViewControllerAsRoot()
-                    return completion(.success)
+                    completion(.success)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.25){
+                        self.router.showQuizzesViewControllerAsRoot()
+                    }
                 }
             }
         }
